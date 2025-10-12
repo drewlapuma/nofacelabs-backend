ca6336a58051aee56e2472f22f393db81f651e50
drewlapuma <drewlapuma@gmail.com>
2025-10-12 00:55:49 -0500

    Update cors.js

--- utils/cors.js
+++ utils/cors.js
@@ -1,16 +1,18 @@
-// utils/cors.js
-export function setCORS(res, origin = 'https://nofacelabsai.webflow.io') {
-  // Use your actual Webflow site origin above.
-  res.setHeader('Access-Control-Allow-Origin', origin);
+// utils/cors.js  (CommonJS)
+const ALLOW_ORIGIN = process.env.ALLOW_ORIGIN || 'https://nofacelabsai.webflow.io';
+
+function setCors(res) {
+  res.setHeader('Access-Control-Allow-Origin', ALLOW_ORIGIN);
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  // If you need cookies across origins, also set:
-  // res.setHeader('Access-Control-Allow-Credentials', 'true');
 }
 
-export const withCORS = (handler, origin = 'https://nofacelabsai.webflow.io') =>
-  async (req, res) => {
-    setCORS(res, origin);
+function allowCors(handler) {
+  return async (req, res) => {
+    setCors(res);
     if (req.method === 'OPTIONS') return res.status(200).end();
     return handler(req, res);
   };
+}
+
+module.exports = { allowCors, setCors };
