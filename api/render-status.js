59fc922ca5ce588c27d1a17dec34dcbd2ee12537
drewlapuma <drewlapuma@gmail.com>
2025-10-12 01:09:00 -0500

    Update render-status.js

--- api/render-status.js
+++ api/render-status.js
@@ -1,16 +1,1 @@
-// api/render-status.js  (CommonJS)
-const { allowCors } = require('../utils/cors');
 
-module.exports = allowCors(async (req, res) => {
-  if (req.method !== 'GET') return res.status(405).json({ error: 'Method Not Allowed' });
-
-  try {
-    const { id } = req.query || {};
-    // TODO: poll Creatomate for the render job status by id
-
-    return res.status(200).json({ ok: true, status: 'pending', id });
-  } catch (err) {
-    console.error('render-status error:', err);
-    return res.status(500).json({ error: 'Server error' });
-  }
-});
