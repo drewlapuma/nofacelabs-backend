ab5e2e6498a677a4f0d566615c90b6f2fa4de796
drewlapuma <drewlapuma@gmail.com>
2026-04-29 22:58:11 -0500

    Create skeleton-reference-images.js

---
+++ api/_lib/skeleton-reference-images.js
@@ -0,0 +1,10 @@
+function getBuiltInSkeletonReferenceImages() {
+  return String(process.env.SKELETON_REFERENCE_IMAGE_URLS || "")
+    .split(",")
+    .map((url) => url.trim())
+    .filter(Boolean);
+}
+
+module.exports = {
+  getBuiltInSkeletonReferenceImages,
+};
